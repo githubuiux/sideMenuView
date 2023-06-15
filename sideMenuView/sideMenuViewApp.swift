2ee80c924ac9ab2b9e9e731652bea13c6b3bb435
githubuiux <uiuxista@gmail.com>
2023-06-15 15:33:37 +0300

    Initial Commit

---
+++ sideMenuView/sideMenuViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  sideMenuViewApp.swift
+//  sideMenuView
+//
+//  Created by Levent on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct sideMenuViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
